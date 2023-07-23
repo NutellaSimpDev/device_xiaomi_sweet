6e5c5e253d8c127989673ac914c3f8e016932dd4
NutellaDev <eduardocayovilca@gmail.com>
2023-07-23 15:59:35 -0500

    sweet: Updating vendorsetup.sh to new trees

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,15 +5,15 @@ git clone https://github.com/NutellaSimpDev/device_xiaomi_sm6150-common.git -b c
 
 echo 'Cloning Vendor tree [2/5]'
 # Device Vendor Tree
-git clone https://github.com/aosp-playground/proprietary_vendor_xiaomi_sweet.git -b thirteen vendor/xiaomi/sweet
+git clone https://github.com/NutellaSimpDev/vendor_xiaomi_sweet.git -b thirteen vendor/xiaomi/sweet
 
 echo 'Cloning Common Vendor tree [3/5]'
 # Common Vendor Tree
-git clone https://github.com/aosp-playground/proprietary_vendor_xiaomi_sm6150-common.git -b thirteen vendor/xiaomi/sm6150-common
+git clone https://github.com/NutellaSimpDev/vendor_xiaomi_sm6150-common.git -b thirteen vendor/xiaomi/sm6150-common
 
 echo 'Cloning Kernel tree [4/5]'
 # Kernel Tree
-git clone https://github.com/vantoman/kernel_xiaomi_sm6150.git -b courbet-13 kernel/xiaomi/sm6150
+git clone https://github.com/vantoman/kernel_xiaomi_sm6150.git -b 13 kernel/xiaomi/sm6150
 
 echo 'Cloning Memecam [5/5]'
 # Memecam
