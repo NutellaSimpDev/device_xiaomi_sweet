f9a8175e3bfe46a850a1054008180622d95e6cae
NutellaDev <133258835+NutellaSimpDev@users.noreply.github.com>
2023-09-20 20:46:56 -0500

    sweet: Update Leica repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,21 +1,21 @@
 echo 'Starting to clone stuffs needed for your device'
-echo 'Cloning Common Tree [1/5]'
+echo 'Cloning Common Tree [1/6]'
 # Sweet-Common Tree
 git clone https://github.com/NutellaSimpDev/device_xiaomi_sm6150-common.git -b crdroid device/xiaomi/sm6150-common
 
-echo 'Cloning Vendor tree [2/5]'
+echo 'Cloning Vendor tree [2/6]'
 # Device Vendor Tree
 git clone https://github.com/NutellaSimpDev/vendor_xiaomi_sweet.git -b thirteen vendor/xiaomi/sweet
 
-echo 'Cloning Common Vendor tree [3/5]'
+echo 'Cloning Common Vendor tree [3/6]'
 # Common Vendor Tree
 git clone https://github.com/NutellaSimpDev/vendor_xiaomi_sm6150-common.git -b thirteen vendor/xiaomi/sm6150-common
 
-echo 'Cloning Kernel tree [4/5]'
+echo 'Cloning Kernel tree [4/6]'
 # Kernel Tree
 git clone https://github.com/vantoman/kernel_xiaomi_sm6150.git -b 13 kernel/xiaomi/sm6150
 
-echo 'Cloning Memecam [5/5]'
+echo 'Cloning Memecam [5/6]'
 # Memecam
-git clone https://github.com/aosp-playground/android_vendor_xiaomi_sweet-miuicamera.git -b thirteen vendor/xiaomi/sweet-miuicamera
+git clone https://gitlab.com/unsatifsed27/miuicamera.git -b leica vendor/xiaomi/sweet-miuicamera
 echo 'Completed, Now proceeding to lunch'
